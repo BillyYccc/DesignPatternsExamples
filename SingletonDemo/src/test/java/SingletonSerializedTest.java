6a976821b1930de719d1647a333b6101a7b740dc
BillyYccc <billy112487983@gmail.com>
2017-04-27 23:53:12 +0800

    提交序列化异常的单例代码

--- SingletonDemo/src/test/java/SingletonSerializedTest.java
+++ SingletonDemo/src/test/java/SingletonSerializedTest.java
@@ -1,5 +1,11 @@
 import org.junit.Test;
 
+import java.io.*;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+
 /**
  * Created by Billy Yuan on 2017/4/27.
  * Email: billy112487983@gmail.com
@@ -7,7 +13,20 @@ import org.junit.Test;
 
 public class SingletonSerializedTest {
     @Test
-    public void singletonSerializedTest(){
+    public void serializedSingletonATest() throws IOException, FileNotFoundException,
+            ClassNotFoundException {
+        SerializeSingletonA instanceA = SerializeSingletonA.getInstance();
+        //Serialize Object to File
+        ObjectOutput out = new ObjectOutputStream(new FileOutputStream("filename.test"));
+        out.writeObject(instanceA);
+        out.close();
 
+        //Deserialize from File to Object
+        ObjectInput in = new ObjectInputStream(new FileInputStream("filename.test"));
+        SerializeSingletonA instanceB = (SerializeSingletonA) in.readObject();
+        in.close();
+
+        //判断两个实例是否相同
+        assertThat(instanceA.hashCode(), not(instanceB.hashCode()));
     }
 }

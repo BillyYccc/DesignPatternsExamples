46665e2c46456fe96922770045c9850d56d57791
BillyYccc <billy112487983@gmail.com>
2017-04-27 23:43:10 +0800

    生成序列化单例测试类

---
+++ SingletonDemo/src/test/java/SingletonSerializedTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by Billy Yuan on 2017/4/27.
+ * Email: billy112487983@gmail.com
+ */
+
+public class SingletonSerializedTest {
+    @Test
+    public void singletonSerializedTest(){
+
+    }
+}

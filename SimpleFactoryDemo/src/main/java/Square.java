7343163fdc54f6fd65d4a6d0bbe869e104efc5b7
BillyYccc <billy112487983@gmail.com>
2017-05-05 18:52:08 +0800

    提交简单工厂模式代码

---
+++ SimpleFactoryDemo/src/main/java/Square.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Billy Yuan on 2017/5/5.
+ * Email: billy112487983@gmail.com
+ */
+
+public class Square extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Draw a square..." + name);
+    }
+
+    public Square(String name) {
+        super(name);
+    }
+}
---
+++ SimpleFactoryDemo/src/test/java/SimpleFactoryTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+/**
+ * Created by Billy Yuan on 2017/5/5.
+ * Email: billy112487983@gmail.com
+ */
+
+public class SimpleFactoryTest {
+    @Test
+    public void simpleFactoryTest() {
+        Shape square1 = SimpleFactory.createShape("Square", "square1");
+        Shape square2 = SimpleFactory.createShape("square", "square2");
+        Shape circle1 = SimpleFactory.createShape("circle", "circle1");
+        Shape circle2 = SimpleFactory.createShape("Circle", "circle2");
+        square1.draw();
+        circle1.draw();
+        square2.draw();
+        circle2.draw();
+    }
+}

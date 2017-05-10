5cabedb043336909a42290b564c5d7ae5dd4281e
BillyYccc <billy112487983@gmail.com>
2017-05-10 10:28:35 +0800

    重构了比较的方式

--- PrototypeDemo/src/main/java/copy/Book.java
+++ PrototypeDemo/src/main/java/copy/Book.java
@@ -26,12 +26,10 @@ public class Book {
     }
 
     public static void compareCopyMethod(Book book1, Book book2) {
-        if ((book1.getPage() == book2.getPage())
-                && (book1.getName().equals(book2.getName()))
-                && (book1.getName() == book2.getName())) {
-            System.out.println("本次拷贝是浅拷贝的...");
+        if (book1.getPage() == book2.getPage() && book1.getName().equals(book2.getName())) {
+            System.out.println(book1.getName() == book2.getName() ? "本次拷贝是浅拷贝的..." : "本次拷贝是深拷贝的...");
         } else {
-            System.out.println("本次拷贝是深拷贝的...");
+            System.out.println("拷贝内容出错...");
         }
     }
 }
